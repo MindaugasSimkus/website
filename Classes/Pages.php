608f7fc9d5d4f0614f2a11a7a7df5d7b1fb72d5e
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-07-19 21:16:41 +0300

    check

--- Classes/Pages.php
+++ Classes/Pages.php
@@ -5,8 +5,13 @@ class Pages {
 	public static function getSinglePage($slug) {
 		$db = new DB();
 		$page = $db->query("SELECT * FROM pages WHERE slug = '$slug'");
+		if ($page != null) {
 		return $page[0];
-	}
+		} else {
+			echo "<h1>Page not found</h1> Go away!";
+			die();
+		}
+ 	}
 
 	public static function storePage($id, $title, $slug, $body, $banner_id) {
 		$db = new DB();
